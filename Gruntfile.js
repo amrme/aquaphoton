37735fc5c388c34484d965f0a9655271961ec998
amrme <amr.e.dev@gmail.com>
2015-11-23 12:51:17 +0200

    added most of packages needed

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,36 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    less: {
+      dev: {
+        options: {
+          sourceMap: true,
+          dumpLineNumbers: 'comments',
+          relativeUrls: true,
+        },
+        files: {
+          'css/bootstrap.css': 'bower_components/components-bootstrap/less/bootstrap.less',
+          'css/main.css': 'less/main.less',
+        },
+      },
+      production: {
+        options: {
+          cleancss: true,
+          compress: true,
+          relativeUrls: true,
+        },
+        files: {
+          'css/bootstrap.css': 'bower_components/components-bootstrap/less/bootstrap.less',
+        },
+      },
+    },
+    cssmin: {
+      target: {
+        files: {
+          'css/main.min.css': ['css/bootstrap.css', 'css/main.css'],
+        },
+      },
+    },
     uglify: {
       my_target: {
         files: {
@@ -12,8 +42,8 @@ module.exports = function(grunt) {
     },
     watch: {
       scripts: {
-        files: ['js/main.js'],
-        tasks: ['uglify'],
+        files: ['js/main.js', 'css/main.css', 'css/bootstrap.css', 'bower_components/components-bootstrap/less/bootstrap.less'],
+        tasks: ['uglify', 'cssmin', 'less'],
         options: {
           spawn: false,
         },
@@ -22,10 +52,11 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['watch']);
-
 };
