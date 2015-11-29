46848ed8ffd31ee7ec0e64071ff69fef7daec22b
amrme <amr.e.dev@gmail.com>
2015-11-29 12:01:11 +0200

    basic services templaet

--- js/main.js
+++ js/main.js
@@ -8,18 +8,47 @@ $(window).load(function() { // makes sure the whole site is loaded
 
 });
 
-// after loading scripts
+// Compile handlebars templates
+var source   = $('#services-template').html();
+var template = Handlebars.compile(source);
+
+// add services
+var context = { services: [{title: 'Service 1',
+                                            img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                        },
+                        {title: 'Service 2',
+                                                                    img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                                                },
+                                                {title: 'Service 2',
+                                                                                            img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                                                                        },
+                                                                        {title: 'Service 2',
+                                                                                                                    img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                                                                                                },
+                                                                                                {title: 'Service 2',
+                                                                                                                                            img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                                                                                                                        },
+                                                                                                                        {title: 'Service 2',
+                                                                                                                                                                    img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                                                                                                                                                },
+                      ], };
+var services    = template(context);
+
+// doc ready
 $(document).ready(function() {
   var currentScroll = 0;
 
   if ($('#first-section').length) {
     $(document).scroll(function() {
       currentScroll = $(this).scrollTop();
-      if (currentScroll + 50 > $('#first-section').offset().top) {
+      if (currentScroll + 10 > $('#first-section').offset().top) {
         $('.navbar-fixed-top').addClass('top-nav-collapse');
       } else {
         $('.navbar-fixed-top').removeClass('top-nav-collapse');
       }
     });
   }
+
+  // append services
+  $('#service').html(services);
 });
