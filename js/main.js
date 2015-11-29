49996c7a4eb20daac8957b847e4eb1fc8837b7e0
amrme <amr.e.dev@gmail.com>
2015-11-29 15:38:48 +0200

    got clicked image id

--- js/main.js
+++ js/main.js
@@ -23,17 +23,17 @@ var context = { services: [
                         },
                         {
                           title: 'Service 2',
-                          img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                          img: 'http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg',
                           modal: 'modal2',
                         },
                         {
                           title: 'Service 3',
-                          img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                          img: 'http://dreamatico.com/data_images/cat/cat-4.jpg',
                           modal: 'modal3',
                         },
                         {
                           title: 'Service 4',
-                          img: 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/siberian-cat1.jpg',
+                          img: 'https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg',
                           modal: 'modal4',
                         },
                           ], };
@@ -58,12 +58,14 @@ $(document).ready(function() {
   $('#service').html(services);
 
   // handle modals for services
-  $('#service-modal').click(function() {
+  $('.service-more-button').click(function() {
+
+    console.log($(this).data('id'));
 
     //use the modal template to generate html
     // and put it in the DOM
-    var html    = modalTemplate(data);
-    $('#service').html(html);
+    var html    = modalTemplate(context);
+    $('#service-modal-content').html(html);
     $('#serviceModal').modal('show');
   });
 });
