3fe1ee4f3e53b7fe541d62579dfeae103a79cc3e
amrme <amr.e.dev@gmail.com>
2015-12-02 04:57:38 +0200

    formated text for services moals

--- js/context.js
+++ js/context.js
@@ -17,12 +17,12 @@ var context = {
                         {
                           title: 'Marine Environmental Services',
                           img: 'img/services/marineEnvServices.jpg',
-                          description: 'AQUAPHOTON ROV provides observation for discovering the aquaculture & monitoring their life. It follows up wastes of factories and ships to avoid harming the aquaculture. AQUAPHOTON ROV characterized by delivering videos with high resolution , easy piloting and working long time underwater.',
+                          description: 'AquaPhoton ROV provides observation for discovering the aquaculture & monitoring their life.<br>It follows up wastes of factories and ships to avoid harming the aquaculture.<br>AquaPhoton ROV characterized by delivering videos with high resolution , easy piloting and working long time underwater.',
                         },
                         {
                           title: 'Science & Research',
                           img: 'img/services/scienceandResearch.jpg',
-                          description: 'Conducting scientific research of aquatic life in the Nile and Lakes like EL-Temsah & EL-Bardawil lakes. Capabilities in collecting samples. Determine metal thickness.',
+                          description: 'Conducting scientific research of aquatic life in the Nile and Lakes like EL-Temsah & EL-Bardawil lakes.<br>Capabilities in collecting samples. Determine metal thickness.',
                         },
                         {
                           title: 'Inspection & Exploration',
@@ -37,17 +37,17 @@ var context = {
                         {
                           title: 'Tanks & Pipelines',
                           img: 'img/services/tankInspection.jpg',
-                          description: 'AQUAPHOTON provides visual inspection for tanks walls, input and output pipes and other obstacles. <br>Also AQUAPHOTON provides sensors inspection like wall thickness gage and Determine any cracks or leakage inside the pipe.',
+                          description: 'AQUAPHOTON provides visual inspection for tanks walls, input and output pipes and other obstacles.<br>Also AQUAPHOTON provides sensors inspection like wall thickness gage and Determine any cracks or leakage inside the pipe.',
                         },
                         {
                           title: 'Ports & Ships',
                           img: 'img/services/port.jpg',
-                          description: 'The safety & security of the ports, harbors and ships is considered to be one of the national security priorities. <br>ROVs have proven their effectiveness to do such missions in many countries around the world, it can be transported and deployed for work in just minutes and can be operated easily by one person only and be used to inspect ship’s hulls and piers and sweep the entire port with no need for divers. <br>Modern sensing nodes and ROVs can carry out their tasks even in the most difficult circumstances such as contaminated water and strong current times. ',
+                          description: 'The safety & security of the ports, harbors and ships is considered to be one of the national security priorities.<br>ROVs have proven their effectiveness to do such missions in many countries around the world, it can be transported and deployed for work in just minutes and can be operated easily by one person only and be used to inspect ship’s hulls and piers and sweep the entire port with no need for divers. <br>Modern sensing nodes and ROVs can carry out their tasks even in the most difficult circumstances such as contaminated water and strong current times. ',
                         },
                         {
                           title: 'Infrastructure',
                           img: 'img/services/infra.jpg',
-                          description: 'Dams, Overwater Bridges, Tanks and Reservoirs are among the most important public infrastructure. Infrastructure faces age’s overtime and needs periodic inspection to ensure safety. AQUAPHOTON provides ROVs equipped with the necessary tools needed to carry out the mission flawlessly. Our engineers are dedicated to do whatever possible to ensure the safety of citizens and infrastructures.',
+                          description: 'Dams, Overwater Bridges, Tanks and Reservoirs are among the most important public infrastructure.<br>Infrastructure faces age’s overtime and needs periodic inspection to ensure safety.<br>AQUAPHOTON provides ROVs equipped with the necessary tools needed to carry out the mission flawlessly.<br>Our engineers are dedicated to do whatever possible to ensure the safety of citizens and infrastructures.',
                         },
                       ],
                         products: [
