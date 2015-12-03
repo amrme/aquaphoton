d1826bc3391f62ff1e86fe7cf1a8aeb9e8847707
amrme <amr.e.dev@gmail.com>
2015-12-03 21:49:04 +0200

    added contact section

--- js/main.js
+++ js/main.js
@@ -97,4 +97,27 @@ $(document).ready(function() {
     $('#service-modal-content').html(html);
     $('#serviceModal').modal('show');
   });
+
+  // contact info
+  var contactform =  document.getElementById('contactform');
+  contactform.setAttribute('action', '//formspree.io/' + 'moataztarek1991' + '@' + 'gmail' + '.' + 'com');
+
+  // map
+  function init_map() {
+    var var_location = new google.maps.LatLng(45.430817,12.331516);
+    var var_mapoptions = {
+       center: var_location,
+       zoom: 14
+    };
+    var var_marker = new google.maps.Marker({
+      position: var_location,
+      map: var_map,
+      title:'Venice'});
+
+    var var_map = new google.maps.Map(document.getElementById("map-container"), var_mapoptions);
+
+   var_marker.setMap(var_map);
+  }
+
+  google.maps.event.addDomListener(window, 'load', init_map);
 });
