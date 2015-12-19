e86e1445859f805a1f761a1cf4578e9c034bb705
amrme <amr.e.dev@gmail.com>
2015-12-20 00:48:31 +0200

    setTimeout for the navbar fixed class to avoid conflict with preloader

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@
 $(window).load(function() { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
   $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  setTimeout(function(){
+       $("nav").addClass("navbar-fixed-top");
+   }, 400);
+
   $('body').delay(350).css({
     overflow: 'visible',
   });
-  $('nav').delay(350).addClass('navbar-fixed-top');
 });
 
 
